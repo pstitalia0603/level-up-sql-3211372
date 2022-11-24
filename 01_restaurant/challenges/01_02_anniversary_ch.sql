179d3ef45c301ef4d664e9cc7821dc793b9a4b63
PSF <48135781+pstitalia0603@users.noreply.github.com>
2022-11-24 15:38:48 +0000

    update 1-4 challenges

--- 01_restaurant/challenges/01_02_anniversary_ch.sql
+++ 01_restaurant/challenges/01_02_anniversary_ch.sql
@@ -5,3 +5,9 @@
 -- they plan to bring in their party.
 
 -- Hint: SQLite offers the INTEGER and REAL datatypes
+
+CREATE TABLE AnniversaryAttendees
+(
+  CustomerID INTEGER,
+  PartySize INTEGER
+);
--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,20 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+
+SELECT *
+FROM Dishes;
+
+SELECT Name, Price
+FROM Dishes
+ORDER BY Price;
+
+Select Name, Type
+FROM Dishes
+WHERE Type ="Appetizer" OR Type="Beverage"
+ORDER BY Type;
+
+SELECT Name, Type
+FROM Dishes
+WHERE TYPE != "Beverage"
+ORDER BY Name;
